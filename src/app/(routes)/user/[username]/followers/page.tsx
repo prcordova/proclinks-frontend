4b2b7e4d5e14322465606a1bf8ff403f60d0bcc4
prcordova <pedroricardocordova@yahoo.com.br>
2025-02-07 23:31:42 -0300

    fix: metadata

--- src/app/(routes)/user/[username]/followers/page.tsx
+++ src/app/(routes)/user/[username]/followers/page.tsx
@@ -1,9 +1,18 @@
+import { Metadata } from 'next'
 import FollowersClient from './followers-client'
 
-export default async function FollowersPage({ 
-  params 
-}: { 
-  params: { username: string } 
-}) {
-  return <FollowersClient username={params.username} />
+type Props = {
+  params: {
+    username: string
+  }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export const metadata: Metadata = {
+  title: 'Seguidores',
+  description: 'Lista de seguidores'
+}
+
+export default function FollowersPage(props: Props) {
+  return <FollowersClient username={props.params.username} />
 }
