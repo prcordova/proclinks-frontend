d95671b6a4be11048bfa59cb1985a3dd7e40e9c8
prcordova <pedroricardocordova@yahoo.com.br>
2025-02-09 00:41:22 -0300

    adds || localhost to api adress

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080',
+  withCredentials: true // Importante para cookies de autenticação
 })
 
 // Interceptor para adicionar o token em todas as requisições
