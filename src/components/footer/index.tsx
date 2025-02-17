7f49d3cf160330acb105f89a112336b748fa2eef
prcordova <pedroricardocordova@yahoo.com.br>
2025-02-17 09:04:12 -0300

    footer links

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,7 +3,7 @@
 import { Box, Container, Typography,   IconButton } from '@mui/material'
  import GitHubIcon from '@mui/icons-material/GitHub'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
-import TwitterIcon from '@mui/icons-material/Twitter'
+ import { Instagram } from '@mui/icons-material'
 
 export function Footer() {
   return (
@@ -36,14 +36,14 @@ export function Footer() {
         
 
           <Box sx={{ display: 'flex', gap: 1 }}>
-            <IconButton color="inherit" href="https://github.com" target="_blank">
+            <IconButton color="inherit" href="https://github.com/prcordova" target="_blank">
               <GitHubIcon />
             </IconButton>
-            <IconButton color="inherit" href="https://linkedin.com" target="_blank">
+            <IconButton color="inherit" href="https://www.linkedin.com/in/pedroricardocordova/" target="_blank">
               <LinkedInIcon />
             </IconButton>
-            <IconButton color="inherit" href="https://twitter.com" target="_blank">
-              <TwitterIcon />
+            <IconButton color="inherit" href="https://www.instagram.com/prcordova/" target="_blank">
+              <Instagram />
             </IconButton>
           </Box>
         </Box>
